c39f56d466d7b95dee0486f80a2e63906e0da27f
Junio Goulart <juniocmgoulart@gmail.com>
2022-07-20 19:58:05 -0300

    kjnfd

--- src/app/hotels/hotels.page.spec.ts
+++ src/app/hotels/hotels.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { ExploreContainerComponent } from './explore-container.component';
+import { HotelsPage } from './hotels.page';
 
-describe('ExploreContainerComponent', () => {
-  let component: ExploreContainerComponent;
-  let fixture: ComponentFixture<ExploreContainerComponent>;
+describe('HotelsPage', () => {
+  let component: HotelsPage;
+  let fixture: ComponentFixture<HotelsPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExploreContainerComponent ],
+      declarations: [ HotelsPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ExploreContainerComponent);
+    fixture = TestBed.createComponent(HotelsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'location',
+        loadChildren: () => import('../location/location.module').then(m => m.LocationPageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'compass',
+        loadChildren: () => import('../compass/compass.module').then(m => m.CompassPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'hotels',
+        loadChildren: () => import('../hotels/hotels.module').then(m => m.HotelsPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/location',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/location',
     pathMatch: 'full'
   }
 ];
